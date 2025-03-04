df692114add1d63f7f8d6deee7edfc38dc49927e
André Höglund <andre.hoglund@learnet.se>
2025-03-04 10:58:20 +0100

    Gjorde en enkel gissnings spel

--- 3.4 main.py
+++ 3.4 main.py
@@ -1,3 +1,25 @@
+import random 
+
 print("Hello Andre!!!")
 print("Hello Alex")
 print("Hello Phurin")
+
+list = random.choice([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+
+x = input("Do you wish to play y/n\n: ")
+if x.lower() == "y":
+    while True:
+        print("How old am i")
+        svar=int(input())
+        if svar < list:
+            print("Too young")
+            continue
+        elif svar > list:
+            print("Too old")
+            continue
+        elif svar == list:
+            print(f"Hurray, you got it right, i am {list} computer years old!")
+            break
+else:
+    print("ok no play then")
+        
